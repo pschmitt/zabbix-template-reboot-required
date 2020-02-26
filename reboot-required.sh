a348975a4acc9063b0f12a12bbea98ac26ee54ce
Philipp Schmitt <philipp@schmitt.co>
2020-02-26 15:45:43 +0100

    Add fedora support

--- reboot-required.sh
+++ reboot-required.sh
@@ -2,148 +2,164 @@
 
 if test -r /etc/os-release
 then
-    . /etc/os-release
+  . /etc/os-release
 elif test -r /etc/turris-version
 then
-    ID=turris
+  ID=turris
 elif test -r /etc/openwrt_version
 then
-    ID=openwrt
+  ID=openwrt
 fi
 
 usage() {
-    echo "Usage: $(basename "$0") [discover|pkg-check]"
+  echo "Usage: $(basename "$0") [discover|pkg-check]"
 }
 
 arch_current_version() {
-    uname -r | sed 's/-[^0-9]*$//'
+  uname -r | sed 's/-[^0-9]*$//'
 }
 
 archarm_current_version() {
-    arch_current_version
+  arch_current_version
 }
 
 turris_current_version() {
-    arch_current_version
+  arch_current_version
 }
 
 openwrt_current_version() {
-    arch_current_version
+  arch_current_version
+}
+
+fedora_current_version() {
+  uname -r | sed -r 's/.fc[0-9]+.*//'
 }
 
 arch_latest_installed() {
-    local package
-    case "$1" in
-        LTS)
-            package=linux-lts
-            ;;
-        VFIO)
-            package=linux-vfio
-            ;;
-        *)
-            package=linux
-            ;;
-    esac
-    pacman -Qi "$package" | awk '/Version/ {print $3}'
+  local package
+  case "$1" in
+    LTS)
+      package=linux-lts
+      ;;
+    VFIO)
+      package=linux-vfio
+      ;;
+    *)
+      package=linux
+      ;;
+  esac
+  pacman -Qi "$package" | awk '/Version/ {print $3}'
 }
 
 archarm_latest_installed() {
-    local package
-    case "$1" in
-        *)
-            package=linux-raspberrypi
-            ;;
-    esac
-    pacman -Qi "$package" | awk '/Version/ {print $3}'
+  local package
+  case "$1" in
+    *)
+      package=linux-raspberrypi
+      ;;
+  esac
+  pacman -Qi "$package" | awk '/Version/ {print $3}'
 }
 
 turris_latest_installed() {
-    opkg list-installed | awk '/kernel - / {print $NF}'
+  opkg list-installed | awk '/kernel - / {print $NF}'
 }
 
 openwrt_latest_installed() {
-    opkg list-installed | awk '/kernel - / {print $NF}' | cut -d - -f 1
+  opkg list-installed | awk '/kernel - / {print $NF}' | cut -d - -f 1
+}
+
+fedora_latest_installed() {
+  dnf list installed | grep "kernel.$(uname -m)" | sort -r | head -1 | \
+    awk '{ print $2 }' | sed -r 's/.fc[0-9]+$//g'
 }
 
 arch_kernel_flavour() {
-    case "$(uname -a)" in
-        *vfio*) echo VFIO ;;
-        *lts*) echo LTS ;;
-        *) echo latest ;;
-    esac
+  case "$(uname -a)" in
+    *vfio*) echo VFIO ;;
+    *lts*) echo LTS ;;
+    *) echo latest ;;
+  esac
 }
 
 archarm_kernel_flavour() {
-    echo latest
+  echo latest
 }
 
 turris_kernel_flavour() {
-    echo latest
+  echo latest
 }
 
 openwrt_kernel_flavour() {
-    echo latest
+  echo latest
+}
+
+fedora_kernel_flavour() {
+  echo latest
 }
 
 kernel_flavour() {
-    case "$ID" in
-        arch|antergos)
-            arch_kernel_flavour
-            ;;
-        archarm)
-            archarm_kernel_flavour
-            ;;
-        turris|openwrt|lede)
-            echo latest
-            ;;
-        *)
-            echo "Unsupported distribution" >&2
-            exit 3
-            ;;
-    esac
+  case "$ID" in
+    arch|antergos)
+      arch_kernel_flavour
+      ;;
+    archarm)
+      archarm_kernel_flavour
+      ;;
+    turris|openwrt|lede|fedora)
+      echo latest
+      ;;
+    *)
+      echo "Unsupported distribution" >&2
+      exit 3
+      ;;
+  esac
 }
 
 package_check() {
-    local current_version
-    local latest_installed_version
-    case "$ID" in
-        arch|antergos)
-            current_version=$(arch_current_version)
-            latest_installed_version=$(arch_latest_installed "$1")
-            ;;
-        archarm)
-            current_version=$(archarm_current_version)
-            latest_installed_version=$(archarm_latest_installed "$1")
-            ;;
-        turris)
-            current_version=$(turris_current_version "$1")
-            latest_installed_version=$(turris_latest_installed "$1")
-            ;;
-        openwrt|lede)
-            current_version=$(openwrt_current_version "$1")
-            latest_installed_version=$(openwrt_latest_installed "$1")
-            ;;
-        *)
-            echo "Unsupported distribution" >&2
-            exit 3
-            ;;
-    esac
-    if test "$current_version" != "$latest_installed_version"
-    then
-        echo "Yes - Kernel update: $current_version -> $latest_installed_version"
-    else
-        echo No
-    fi
+  local current_version
+  local latest_installed_version
+  case "$ID" in
+    arch|antergos)
+      current_version=$(arch_current_version)
+      latest_installed_version=$(arch_latest_installed "$1")
+      ;;
+    archarm)
+      current_version=$(archarm_current_version)
+      latest_installed_version=$(archarm_latest_installed "$1")
+      ;;
+    turris)
+      current_version=$(turris_current_version "$1")
+      latest_installed_version=$(turris_latest_installed "$1")
+      ;;
+    openwrt|lede)
+      current_version=$(openwrt_current_version "$1")
+      latest_installed_version=$(openwrt_latest_installed "$1")
+      ;;
+    fedora)
+      current_version=$(fedora_current_version "$1")
+      latest_installed_version=$(fedora_latest_installed "$1")
+      ;;
+    *)
+      echo "Unsupported distribution" >&2
+      exit 3
+      ;;
+  esac
+  if test "$current_version" != "$latest_installed_version"
+  then
+    echo "Yes - Kernel update: $current_version -> $latest_installed_version"
+  else
+    echo No
+  fi
 }
 
 case "$1" in
-    kernel-flavour)
-        kernel_flavour
-        ;;
-    update-available)
-        ;;
-    *)
-        package_check "$(kernel_flavour)"
-        ;;
+  kernel-flavour)
+    kernel_flavour
+    ;;
+  *)
+    package_check "$(kernel_flavour)"
+    ;;
 esac
 
+# vim set ft=sh et ts=2 sw=2 :
